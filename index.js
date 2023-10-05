5a57521f35247d42e45410b7a161c015c3a9c9c2
joy Kurui <kurui.chepngetich@gmail.com>
2023-10-05 15:47:22 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
-// Write your code here
+// Write your c
+let num1 = 31;
+let num2 = 2;
+let multiply = num1*num2;
+ console.log(multiply);
+
+ let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 14
+let num4 = 5
+ let mod  = num3%num4
+
+let max = Math.max(2,4,6,10,12,14,16,18,20);
